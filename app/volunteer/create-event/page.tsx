bfdfd570855213f9c81c51dd19ee71f56916b719
splimon <splimon@hawaii.edu>
2025-11-06 13:22:44 -1000

    Add Markdown Editor

--- app/volunteer/create-event/page.tsx
+++ app/volunteer/create-event/page.tsx
@@ -1,14 +1,17 @@
 'use client';
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+// @ts-expect-error - CSS module types may be missing in the project; to properly fix this add a declaration file (e.g. global.d.ts) with: declare module '*.css';
+import "easymde/dist/easymde.min.css";
+
 
 const NewEventPage = () => {
-
   return (
     <div className="max-w-xl space-y-3">
         <h1>Create New Volunteer Event</h1>
         <TextField.Root placeholder="Event Title" variant="classic" />
-        <TextArea resize="vertical" placeholder="Event Description" variant="classic" />
+        <SimpleMDE placeholder="Event Description"/>
         <TextField.Root placeholder="Event Date" variant="classic" />
         <TextField.Root placeholder="Event Time" variant="classic" />
         <TextField.Root placeholder="Event Location" variant="classic" />
