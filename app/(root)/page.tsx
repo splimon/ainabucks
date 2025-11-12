5ba17629c8acf7479ba88c64a36928a7f7007466
splimon <splimon@hawaii.edu>
2025-11-12 00:30:32 -1000

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,14 @@
 import MainHome from "@/components/home/MainHome";
+import { db } from "@/database/drizzle";
+import { usersTable } from "@/database/schema";
 
-const Home = () => <MainHome />;
+const Home = async () =>  {
+    const result = await db.select().from(usersTable);
+    console.log(JSON.stringify(result, null, 2));
+
+    return (
+        <MainHome />
+    );
+};
 
 export default Home;
