fe785ed7d0a7e4aae62e0afa3c7f3843c8196ccd
splimon <splimon@hawaii.edu>
2025-11-13 12:29:20 -1000

    Implement rate limiting

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,24 @@
+/*
+* /app/too-fast/page.tsx
+* Displays a message to users who are making requests too quickly.
+* Used in conjunction with rate limiting to inform users to slow down.
+*/
+
+import React from 'react'
+
+const TooFast = () => {
+  return (
+    <div className='flex flex-col justify-center items-center px-6 min-h-screen bg-linear-to-b from-green-50 to-green-100 py-6'>
+        <h1 className='text-4xl font-bold leading-tight mb-3'>
+            Whoa, too fast! Please slow down.
+        </h1>
+        <p>
+        You&apos;ve made too many requests too quickly. We&apos;ve put a
+        temporary pause on your excitement.
+      </p>
+      <p>🚦 Chill for a bit, and try again shortly</p>
+    </div>
+  )
+}
+
+export default TooFast
--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -3,6 +3,7 @@
  *
  * This file contains server-side actions for user authentication, including sign-up and sign-in functionalities.
  * It interacts with the database to create new users and validate existing ones.
+ * It also incorporates rate limiting to prevent DDoS attacks during the sign-in/sign-up process.
  */
 
 "use server";
@@ -12,12 +13,21 @@ import { usersTable } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { hash, genSalt } from "bcryptjs";
 import { signIn } from "@/app/(root)/auth";
+import { headers } from "next/headers";
+import ratelimit from "@/lib/ratelimit";
+import { redirect } from "next/navigation";
 
 /* Helper function to sign in user after sign up */
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
 ) => {
   const { email, password } = params;
+
+  // Get user IP address from headers for rate limiting or logging purposes
+  const ip = (await headers()).get("x-forwarded-for") || "unknown";
+  const {success} = await ratelimit.limit(ip); // Implement rate limiting logic as needed
+
+  if (!success) return redirect('/too-fast');
 
   try {
     const result = await signIn("credentials", {
@@ -42,6 +52,12 @@ export const signInWithCredentials = async (
 /* Main sign up function */
 export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, password, confirmPassword } = params;
+
+  // Get user IP address from headers for rate limiting or logging purposes
+  const ip = (await headers()).get("x-forwarded-for") || "unknown";
+  const {success} = await ratelimit.limit(ip); // Implement rate limiting logic as needed
+
+  if (!success) return redirect('/too-fast');
 
   // Check if the user already exists
   const existingUser = await db
