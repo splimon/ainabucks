31d4ce1d45a082ae698619062c90881762a5a91c
splimon <splimon@hawaii.edu>
2025-11-13 13:32:21 -1000

    Add production API endpoint

--- lib/config.ts
+++ lib/config.ts
@@ -5,6 +5,7 @@
 
 const config = {
   env: {
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
