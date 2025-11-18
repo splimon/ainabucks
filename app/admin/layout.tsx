3924b67ebb52a3c3df7d2ac59cb9317e40898c66
splimon <splimon@hawaii.edu>
2025-11-18 12:11:52 -1000

    Add admin permissions for accessing admin page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,7 +3,9 @@ import { auth } from '../(root)/auth';
 import { redirect } from 'next/navigation';
 import Sidebar from '@/components/admin/Sidebar';
 import Header from '@/components/admin/Header';
-
+import { db } from '@/database/drizzle';
+import { usersTable } from '@/database/schema';
+import { eq } from "drizzle-orm";
 
 const AdminLayout = async ({children} : {children: ReactNode}) => {
   const session = await auth();
@@ -12,6 +14,16 @@ const AdminLayout = async ({children} : {children: ReactNode}) => {
   if (!session?.user?.id) redirect('/sign-in');
 
 
+  const isAdmin = await db
+    .select({ isAdmin: usersTable.role })
+    .from(usersTable)
+    .where(eq(usersTable.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  if (!isAdmin) redirect("/");
+
+  
   return (
     <main className='flex min-h-screen w-full flex-row'>
       <Sidebar session={session}/>
